8bdd6fdfba3f68e224289688cd934160411bb6fb
Dwarfius <youman6@gmail.com>
2014-03-17 16:51:19 +0000

    improved reverse for AI

--- Assets/Scripts/SteeringAI.cs
+++ Assets/Scripts/SteeringAI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class SteeringAI : Car 
 {
+    const float timerConst = 1.25f;
     public bool userOverride;
     int currentWaypoint = 0;
     Line currentSegm;
@@ -51,10 +52,8 @@ public class SteeringAI : Car
         {
             if ((projectedPos.ToV2() - newPoint).sqrMagnitude > rad * rad)
             {
-                float dist = (projectedPos.ToV2() - newPoint).magnitude;
-                //throttle = (1 / (dist - rad));
-                throttle = 0.1f;
-                steer = currentSegm.IsLeftOfLine(projectedPos.ToV2()) * Mathf.Clamp01((dist - rad)/ (rad * 3));
+                throttle = 0;
+                steer = currentSegm.IsLeftOfLine(projectedPos.ToV2());
                 color = Color.red;
             }
             else
@@ -66,22 +65,41 @@ public class SteeringAI : Car
         }
         else
         {
-            timer -= Time.deltaTime;
-            throttle = -0.1f;
-            steer = -currentSegm.IsLeftOfLine(projectedPos.ToV2());
+            if (timer > timerConst / 4) //75% spent on reverse
+            {
+                throttle = -0.1f;
+                steer = -currentSegm.IsLeftOfLine(projectedPos.ToV2());
+            }
+            else //remaining %25 on repositioning
+            {
+                if ((projectedPos.ToV2() - newPoint).sqrMagnitude > rad * rad)
+                {
+                    throttle = 0;
+                    steer = currentSegm.IsLeftOfLine(projectedPos.ToV2());
+                    color = Color.red;
+                }
+                else
+                {
+                    throttle = 1;
+                    steer = 0;
+                    color = Color.green;
+                }
+            }
             color = Color.blue;
+            timer -= Time.deltaTime;
         }
         Debug.DrawLine(new Vector3(projectedPos.x, transform.position.y, projectedPos.z), new Vector3(newPoint.x, transform.position.y, newPoint.y), color);
     }
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag != "Ground")
+        if (other.gameObject.tag == "Wall")
         {
+            Debug.Log("Hit");
             foreach (ContactPoint p in other.contacts)
             {
                 Debug.DrawLine(p.point, p.point + p.normal, Color.red, 3);
-                timer = 2;
+                timer = timerConst;
             }
         }
     }
@@ -94,13 +112,13 @@ public class SteeringAI : Car
             if (timer <= 0 && Mathf.Abs(transform.forward.z - currentSegm.ForwardNormal.z) > 1) //if going the other direction
             {
                 t += Time.fixedDeltaTime;
-                if (t >= 1)
+                if (t >= timerConst)
                 {
                     t = 0;
-                    transform.position = currentSegm.aTrans.position;
-                    transform.LookAt(currentSegm.bTrans);
                     rigidbody.velocity = Vector3.zero;
                     rigidbody.angularVelocity = Vector3.zero;
+                    transform.position = currentSegm.aTrans.position;
+                    transform.LookAt(currentSegm.bTrans);
                 }
             }
             yield return new WaitForFixedUpdate();
